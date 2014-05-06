3b9296018fb8b177dc3a28004824f0b0f530b64a
slguan <guan.shiliang@icntv.tv>
2014-05-06 11:52:28 +0800

    fix bug

--- Classes/Common/RandomImpl.cpp
+++ Classes/Common/RandomImpl.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <iostream>
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "RandomImpl.h"
 
--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -8,8 +8,11 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
+                   ../../Classes/Common/RandomImpl.cpp \
                    ../../Classes/Scene/Title/TitleScene.cpp \
                    ../../Classes/Scene/Game/GameScene.cpp \
+                   ../../Classes/Scene/Game/Mana.cpp \
+                   ../../Classes/Scene/Game/Burger.cpp \
                    ../../Classes/Scene/Result/ResultScene.cpp \
                    hellocpp/Purchase.cpp \
                    hellocpp/Store.cpp \
@@ -19,13 +22,18 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    hellocpp/LBFileUtils.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/Common
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/Scene
+
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/cocos/ui
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/cocos/editor-support
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_ui_static
 
 
@@ -36,4 +44,5 @@ $(call import-module,audio/android)
 $(call import-module,Box2D)
 $(call import-module,editor-support/cocosbuilder)
 $(call import-module,editor-support/cocostudio)
+$(call import-module,editor-support/spine)
 $(call import-module,ui)
